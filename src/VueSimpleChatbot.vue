307479df84fb904f10e6d5943e77a4bbfc4233ea
Pierre Charles <pierre.chls@gmail.com>
2017-11-01 11:24:21 +0100

    :tada: Adding source

---
+++ src/VueSimpleChatbot.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="simple-chatbot-container">
+    <!-- Here the simple chatbot container -->
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {},
+    data () {
+      return {}
+    },
+    computed: {},
+    methods: {},
+    watch: {},
+    created () {}
+  }
+
+</script>
+
+<style lang="scss">
+
+  body {
+    background: red !important;
+
+    h1 {
+      color: green;
+    }
+  }
+
+</style>
