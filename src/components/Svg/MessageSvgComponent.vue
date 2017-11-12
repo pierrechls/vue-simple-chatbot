c2e743035a1ba6b04be191e13251743f96c661e3
Pierre Charles <pierre.chls@gmail.com>
2017-11-12 22:05:16 +0100

    :tada: Creating some SVG components

---
+++ src/components/Svg/MessageSvgComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg :style="`fill:${color}`" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>
+</template>
+
+<script>
+
+    export default {
+        props: {
+          color: {
+            type: String,
+            required: true
+          }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Svg/SendSvgComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg :style="`fill:${color}`" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 500 500"><g><g><polygon points="0,497.25 535.5,267.75 0,38.25 0,216.75 382.5,267.75 0,318.75"></polygon></g></g></svg>
+</template>
+
+<script>
+
+    export default {
+        props: {
+          color: {
+            type: String,
+            required: true
+          }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
